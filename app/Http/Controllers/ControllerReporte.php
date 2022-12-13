ae5b9c823c38a8ea91a7256fee6b03e67b6b3a9b
unknown <tic.fhce@gmail.com>
2022-12-13 14:01:59 -0400

    Quinto Commit Marcelo Quiroga

--- app/Http/Controllers/ControllerReporte.php
+++ app/Http/Controllers/ControllerReporte.php
@@ -9,7 +9,7 @@ use App\Models;
 class ControllerReporte extends Controller
 {
     //
-    public function reporte(Request $resquest){
+    public function reporte2(Request $resquest){
         session_start();
     	if(isset($_SESSION['usuario']))
         {
@@ -18,18 +18,19 @@ class ControllerReporte extends Controller
             $sub=Models\Submenu::get();
             $dato=Models\Datos::where('id_persona',$resquest->id_persona)->first();
             
-            
-
-
-            $bruto2=Models\Bruto::where('user_id',$dato->user_id)->where('gestion',$resquest->gestion)->where('mes',$resquest->mes)->where('lugar','MQ')->get();
+            $bto=Models\Bruto::where('user_id',$dato->user_id)->where('gestion',$resquest->gestion)->where('mes',$resquest->mes)->where('lugar','MQ')->get();
             $bruto=[];
+            $bruto2=[];
             $pares=[];
             $aux='';
+            foreach($bto as $b)
+                array_push($bruto2,$b);    
+            array_push($bruto2,$bruto2[0]);
             foreach($bruto2 as $b){
                 echo($aux.'<br>');
                 if($aux==''){
                     $aux=$b->fecha;
-                    array_push($pares,$b);
+                    array_push($pares,$b);        
                 }
                 else{
                     if($aux==$b->fecha)
@@ -74,6 +75,76 @@ class ControllerReporte extends Controller
 
                 
                 
+            }
+            
+            $titulo="SCC/Lista de Personal";
+            //$titulo2=$datos->usser;
+            return view('reporte.lista_reporte',compact('titulo','usuario','menu','sub','bruto'));
+        }
+    	else
+        	return redirect('/');
+    }
+    public function reporte(Request $resquest){
+        session_start();
+    	if(isset($_SESSION['usuario']))
+        {
+            $usuario=$_SESSION['usuario'];
+            $menu=DB::table('viewmenu')->where('id_usuario',$usuario->id)->get();
+            $sub=Models\Submenu::get();
+            $dato=Models\Datos::where('id_persona',$resquest->id_persona)->first();
+            
+            $bto=Models\Bruto::where('user_id',$dato->user_id)->where('gestion',$resquest->gestion)->where('mes',$resquest->mes)->where('lugar','MQ')->orderby('fecha','asc')->get();
+            $bruto=[];
+            $bruto2=[];
+            $pares=[];
+            $aux='';
+            foreach($bto as $b)
+                array_push($bruto2,$b);    
+            array_push($bruto2,$bruto2[0]);
+            foreach($bruto2 as $b){
+                if($aux==''){
+                    $aux=$b->fecha;
+                    array_push($pares,$b);        
+                }
+                else{
+                    if($aux==$b->fecha)
+                        array_push($pares,$b);
+                    else{
+                        $aux=$b->fecha;
+                        $entrada=[];
+                        $salida=[];
+                        foreach($pares as $n){
+                            $e=(int)$n->h*100+$n->m;
+                            echo($n->fecha.' '.$e);
+                            if($e<1030){
+                                array_push($entrada,$n);
+                                echo('Entrada <br>');
+                            }
+                                
+                            else{
+                                array_push($salida,$n);
+                                echo('Salida <br>');
+                            }
+                                
+                        }
+                        if(count($entrada)>0) 
+                            array_push($bruto,$entrada[count($entrada)-1]);                            
+                        else {
+                            $a= clone ($salida[0]);
+                            $a->hora='';
+                            array_push($bruto,$a);    
+                        }
+                        if(count($salida)>0)
+                            array_push($bruto,$salida[0]);
+                        else{
+                            $c= clone ($entrada[count($entrada)-1]);
+                            $c->hora='';
+                            array_push($bruto,$c);
+                        }
+                        $pares=[];
+                        array_push($pares,$b);
+                    }
+                }
             }
             
             $titulo="SCC/Lista de Personal";
